3e6323346ed00a920aee6ca8f6eb0f017788f09b
Kamazz1k <144707479+Kamazz1k@users.noreply.github.com>
2024-01-25 19:39:10 +0500

    Task 2

---
+++ Tyuiu.TolstykhVS.Sprint2.Task2.V30.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TolstykhVS.Sprint2.Task2.V30.Lib;
+
+namespace Tyuiu.TolstykhVS.Sprint2.Task2.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 4, y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+
+}
---
+++ Tyuiu.TolstykhVS.Sprint2.Task2.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TolstykhVS.Sprint2.Task2.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TolstykhVS.Sprint2.Task2.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc26740b-3814-4420-a78d-fde0ddda3775")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
